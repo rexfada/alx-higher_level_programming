5e96540c4b00faa75535d1f883a3cf34a83c91c2
rexfada <rexfada@gmail.com>
2023-03-11 23:19:36 +0100

    magic calculation

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+    else:
+        c = sub(a, b)
+    return c
