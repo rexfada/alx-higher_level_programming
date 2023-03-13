42dbca18c3e7960c6473e6f0d4ad2709615812b0
rexfada <rexfada@gmail.com>
2023-03-13 21:28:09 +0100

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
-#!usr/bin/python3
-def new_in_list(my_list, idx, element):
-    def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]  # return a copy of the original list
+#!/usr/bin/python3
 
-    new_list = my_list[:]  # create a copy of the original list
-    new_list[idx] = element  # modify the copy
-    return new_list
+
+def new_in_list(my_list, idx, element):
+
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
