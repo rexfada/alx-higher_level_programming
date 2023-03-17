b9efd63d7103add6f94a902868ed5aa1fa564e1c
rexfada <rexfada@gmail.com>
2023-03-17 19:15:35 +0100

    union

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_3 = set_1.union(set_2)
+    dif_1 = set_1.difference(set_2)
+    dif_2 = set_2.difference(set_1)
+    set_3 = dif_1.union(dif_2)
     return set_3
