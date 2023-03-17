81dce5ce757aa91f6c457a80932b075f46c2a981
rexfada <rexfada@gmail.com>
2023-03-17 19:44:15 +0100

    sorted

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_dict =  sorted(a_dictionary.keys())
+    for key in sorted_dict:
+        value = a_dictionary[key]
+        if isinstance(value, dict):
+            print(f"{key}: ")
+            print_sorted_dictionary(value)
+        else:
+            print(f"{key}: {value}")
